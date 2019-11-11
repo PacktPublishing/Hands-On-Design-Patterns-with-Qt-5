9cc026119afc49f2ae7a3bb4d2ba0ff3fe83b3d8
John Werner <jwernerny@gmail.com>
2019-11-11 15:44:22 -0500

    Adding IntrospectionExample

---
+++ ch02/IntrospectionExample/Person.h
@@ -0,0 +1,17 @@
+#ifndef MYCLASS_H
+#define MYCLASS_H
+
+#include <QObject>
+
+class MyClass : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MyClass(QObject *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // MYCLASS_H
