77c82f204accccf47e0b6d1685e038cd2c5b8a25
John Werner <jwernerny@gmail.com>
2019-11-11 15:46:01 -0500

    Added IntrospectionExample

--- ch02/IntrospectionExample/Person.cpp
+++ ch02/IntrospectionExample/Person.cpp
@@ -1,6 +1,46 @@
-#include "MyClass.h"
+#include "Person.h"
 
-MyClass::MyClass(QObject *parent) : QObject(parent)
+Person::Person(QObject *parent) : QObject(parent)
 {
 
+}
+
+QString Person::name() const
+{
+    return m_name;
+}
+
+void Person::setName(const QString &name)
+{
+    m_name = name;
+}
+
+QDate Person::birthday() const
+{
+    return m_birthday;
+}
+
+void Person::setBirthday(const QDate &birthday)
+{
+    m_birthday = birthday;
+}
+
+int Person::heightCm() const
+{
+    return m_heightCm;
+}
+
+void Person::setHeightCm(int heightCm)
+{
+    m_heightCm = heightCm;
+}
+
+Person::SexEnum Person::sex() const
+{
+    return m_sex;
+}
+
+void Person::setSex(Person::SexEnum sex)
+{
+    m_sex = sex;
 }
--- ch02/IntrospectionExample/Person.h
+++ ch02/IntrospectionExample/Person.h
@@ -1,17 +1,53 @@
-#ifndef MYCLASS_H
-#define MYCLASS_H
+#ifndef PERSON_H
+#define PERSON_H
 
 #include <QObject>
+#include <QDate>
+#include <QString>
 
-class MyClass : public QObject
+class Person : public QObject
 {
     Q_OBJECT
+
 public:
-    explicit MyClass(QObject *parent = nullptr);
+    enum SexEnum {
+        MALE,
+        FEMALE
+    };
+
+    Q_ENUM(SexEnum)
+
+    Q_PROPERTY(QString name     READ name     WRITE setName)
+    Q_PROPERTY(QDate   birthday READ birthday WRITE setBirthday)
+    Q_PROPERTY(int     heightCm READ heightCm WRITE setHeightCm)
+    Q_PROPERTY(SexEnum sex      READ sex      WRITE setSex)
+
+public:
+    explicit Person(QObject *parent = nullptr);
+
+
+    QString name() const;
+    void setName(const QString &name);
+
+    QDate birthday() const;
+    void setBirthday(const QDate &birthday);
+
+    int heightCm() const;
+    void setHeightCm(int heightCm);
+
+    SexEnum sex() const;
+    void setSex(SexEnum sex);
 
 signals:
 
 public slots:
+
+private:
+    QString     m_name;     ///< name of the person
+    QDate       m_birthday; ///< the person's birthday
+    int         m_heightCm; ///< the person's height (in cm)
+    SexEnum     m_sex;      ///< the person's sex
+
 };
 
-#endif // MYCLASS_H
+#endif // PERSON_H
