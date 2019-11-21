2f2b9a89e50f322934acebee154bda43df101eda
John Werner <jwernerny@gmail.com>
2019-11-21 13:15:03 -0500

    Added comments to main.cpp in DesignByWidgets

--- ch02/WidgetByDesign/main.cpp
+++ ch02/WidgetByDesign/main.cpp
@@ -4,8 +4,8 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QApplication a(argc, argv); // (1) create a Widgets application
+    MainWindow w;               // (2) create the MainWindow
+    w.show();                   // (3) show it
+    return a.exec();            // (4) start the application
 }
